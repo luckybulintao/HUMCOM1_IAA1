b074d5a24380aaba4f522df7f3a7c0966abd9d32
LuckyBulintao/HUMCOM1_IAA1 <98396333+luckybulintao@users.noreply.github.com>
2022-05-14 00:31:43 -0800

    Add files via upload

---
+++ Exercise 15- 17Variables and Types/JS_Folder/Script15.js
@@ -0,0 +1,11 @@
+let name="Lucky";
+let age="19";
+let phonenumber="09776027256"
+let emailaddress="20216983@s.ubaguio.edu";
+let mailingaddress="3600 Lagawe, Ifugao";
+let program="Computer Science";
+let idnumber="20216983";
+let yearlevel="1st Year";
+let section="IAA1";
+
+        
